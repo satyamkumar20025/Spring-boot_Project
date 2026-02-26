9a0f585fc85151ee2149451d858a98f731db85ad
satyamkumar20025 <satyamkumar6372@gmail.com>
2026-02-26 09:45:54 +0530

    spring-boot_Project

---
+++ src/main/java/com/example/employee/EmployeeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.employee;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApplicationTests {
+
+	public static void main(String[]args)
+	{
+         SpringApplication.run(EmployeeApplicationTests.class,args);
+	}
+		
+	}
+
